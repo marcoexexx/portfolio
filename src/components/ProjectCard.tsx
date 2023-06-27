844a0292291ea2be74025c491749dab422d37142
marco <toyko20012gmail.com>
2023-06-27 17:16:18 +0630

    lineheight

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -38,7 +38,7 @@ export default function ProjectCard(props: ProjectCardProps) {
           lineHeight="1.3rem"
         />
       </Link>
-      {showDescription && <Text lineHeight="1.2rem" fontFamily="heading">
+      {showDescription && <Text lineHeight="1.33rem" fontFamily="heading">
         {getDescription()}
         {link && <IconButton onClick={linkOpen(link)} size='xs' ml={1} variant='text' icon={<FiExternalLink size="1rem" />} aria-label="external link" />}
       </Text>}
