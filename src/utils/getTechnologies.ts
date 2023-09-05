bc5ae3af630976032559c91b865296884110fbc7
marco <toyko20012gmail.com>
2023-09-05 15:41:41 +0630

    remove kr handbook project -- update

--- src/utils/getTechnologies.ts
+++ src/utils/getTechnologies.ts
@@ -1,3 +1,4 @@
 export function getTechnology(key: string) {
   return `https://github.com/topics/${key}`;
+  // return `https://github.com/alk-neq-me?tab=repositories&q=&type=&language=${key}&sort=`
 }
